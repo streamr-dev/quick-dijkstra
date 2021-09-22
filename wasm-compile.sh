d49efcf9ad279c80ffb66bc0de7430b8d2ff145f
Petri Savolainen <petri.savolainen@helsinki.fi>
2021-09-22 14:11:23 +0300

    Add allow memory growth flag to wasm compile script

--- wasm-compile.sh
+++ wasm-compile.sh
@@ -5,5 +5,5 @@ cp ${pwd}/src/wasmjs/quickdijkstra-wasm.js ${pwd}/wasmdist
 cp ${pwd}/src/wasmjs/quickdijkstra-wasm.d.ts ${pwd}/wasmdist
 cp ${pwd}/src/wasmjs/wasm_package.json ${pwd}/wasmdist && mv ${pwd}/wasmdist/wasm_package.json ${pwd}/wasmdist/package.json
 emcc --bind -O2 ${pwd}/src/cpp/dijkstraengine.cc -o ${pwd}/wasmdist/dijkstraengine.js  \
--s WASM=1 -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['addOnPostRun']" -s ASSERTIONS=1 \
+-s WASM=1 -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['addOnPostRun']" -s ASSERTIONS=1 -s ALLOW_MEMORY_GROWTH=1 \
 || exit 1
